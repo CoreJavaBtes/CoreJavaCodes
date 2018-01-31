a230ee6885d57ac8746f3ef41e8f6d713fe719d2
Sipika <CoreJavaBtes@gmail.com>
2018-01-31 14:54:30 +0530

    fd

---
+++ src/Demo/CodilityArrayTest.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class CodilityArrayTest {
+	
+	public int solution(int[] A){
+		
+		int index =0;
+	
+		
+		
+		return A[index];
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
