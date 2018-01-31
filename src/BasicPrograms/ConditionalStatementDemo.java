b6d91aef5dbb2029c94c206d8da3983ba3fac169
Sipika <CoreJavaBtes@gmail.com>
2018-01-31 14:51:48 +0530

    gg

---
+++ src/BasicPrograms/ConditionalStatementDemo.java
@@ -0,0 +1,18 @@
+package BasicPrograms;
+
+public class ConditionalStatementDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 9;
+		if(a>b){
+			System.out.println(a);
+			
+		}else{
+			System.out.println(b);
+		}
+	}
+
+}
